94d3cccc58de5dd69162f209fe73be57c88f5ead
Behzod101 <soataliyevbehzod45@gmail.com>
2024-01-19 19:17:53 +0500

    landing page restaraunt

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Testimonal from './components/Testimonal';
+
+function App() {
+    return (
+        <div className="App">
+            <Home/>
+            <About/>    
+            <Testimonal/>
+        </div>
+    );
+}
+
+export default App;
